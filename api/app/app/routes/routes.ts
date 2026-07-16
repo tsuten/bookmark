6ce8854f6da0b0f33a6e33363a4c0e0eed270986
Tsuten <hiimhereforyouguys@gmail.com>
2026-07-16 15:28:11 +0900

    fixed routes inspecting endpoint

--- api/app/app/routes/routes.ts
+++ api/app/app/routes/routes.ts
@@ -1,7 +1,16 @@
 import { createRoute } from 'honox/factory'
-import { showRoutes } from 'hono/dev'
+import { inspectRoutes } from 'hono/dev'
 import app from '../server'
 
+function formatRoutes(): string {
+  const routes = inspectRoutes(app).filter(({ isMiddleware }) => !isMiddleware)
+  const maxMethodLength = Math.max(0, ...routes.map((r) => r.method.length))
+
+  return routes
+    .map(({ method, path }) => `${method.padEnd(maxMethodLength)} ${path}`)
+    .join('\n')
+}
+
 export const GET = createRoute(async (c) => {
-  return c.text(showRoutes(app))
+  return c.text(formatRoutes())
 })
